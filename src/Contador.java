93e97840001e1e04c0e814044611f65fc744cebe
YannFigueiredo <yann.fabricio@hotmail.com>
2024-05-10 15:49:38 -0300

    feat: add counting code

---
+++ src/Contador.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        int primeiroParametro;
+        int segundoParametro;
+
+        System.out.println("Informe o primeiro parâmnetro: ");
+        primeiroParametro = scanner.nextInt();
+
+        System.out.println("Informe o segundo parâmetro: ");
+        segundoParametro = scanner.nextInt();
+
+        try{
+            contar(primeiroParametro, segundoParametro);
+        }catch(ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+    }
+
+    public static void contar(int primeiroParametro, int segundoParametro) throws ParametrosInvalidosException {
+        if(primeiroParametro > segundoParametro)
+            throw new ParametrosInvalidosException();
+
+        int limiteContador = segundoParametro - primeiroParametro;
+
+        for(int i = 0; i < limiteContador; i++) {
+            System.out.println("Imprimindo o número " + (i+1) + ".");
+        }
+    }
+}
